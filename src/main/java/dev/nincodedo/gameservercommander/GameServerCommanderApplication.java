45f85abfc9969c7e56dae910969a7f9f661c73f6
Nincodedo <nincodedo@gmail.com>
2021-02-26 20:49:33 -0600

    Initial

---
+++ src/main/java/dev/nincodedo/gameservercommander/GameServerCommanderApplication.java
@@ -0,0 +1,13 @@
+package dev.nincodedo.gameservercommander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameServerCommanderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GameServerCommanderApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/nincodedo/gameservercommander/GameServerCommanderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nincodedo.gameservercommander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServerCommanderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
