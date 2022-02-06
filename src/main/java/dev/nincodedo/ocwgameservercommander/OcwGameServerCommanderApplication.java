cf7a3276cd8bd758db33ff68941283a333c2baf1
Nincodedo <nincodedo@gmail.com>
2022-02-06 01:22:41 -0600

    Initial commit

---
+++ src/main/java/dev/nincodedo/ocwgameservercommander/OcwGameServerCommanderApplication.java
@@ -0,0 +1,13 @@
+package dev.nincodedo.ocwgameservercommander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcwGameServerCommanderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcwGameServerCommanderApplication.class, args);
+    }
+
+}
