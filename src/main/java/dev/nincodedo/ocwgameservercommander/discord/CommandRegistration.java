f31c0ac4c4b08b8a9f14cd3ea8b7c0875f41973c
Nincodedo <nincodedo@gmail.com>
2022-10-10 10:23:51 -0500

    Fix import

--- src/main/java/dev/nincodedo/ocwgameservercommander/discord/CommandRegistration.java
+++ src/main/java/dev/nincodedo/ocwgameservercommander/discord/CommandRegistration.java
@@ -2,7 +2,7 @@ package dev.nincodedo.ocwgameservercommander.discord;
 
 import dev.nincodedo.ocwgameservercommander.config.Constants;
 import lombok.extern.slf4j.Slf4j;
-import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
