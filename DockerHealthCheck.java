afc30f55dbab79bb2f2129efc1902c33f894823e
Nincodedo <nincodedo@gmail.com>
2023-01-24 13:19:21 -0600

    Change port

--- DockerHealthCheck.java
+++ DockerHealthCheck.java
@@ -7,7 +7,7 @@ public class DockerHealthCheck {
   public static void main(String[] args) throws InterruptedException, IOException {
     var client = HttpClient.newHttpClient();
     var request = HttpRequest.newBuilder()
-        .uri(URI.create("http://localhost:8080/actuator/health"))
+        .uri(URI.create("http://localhost:8085/actuator/health"))
         .header("accept", "application/json")
         .build();
     var response = client.send(request, BodyHandlers.ofString());
